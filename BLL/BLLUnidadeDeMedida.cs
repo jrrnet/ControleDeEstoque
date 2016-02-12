32c4f72cbaa0da03e338426339cc440e3cc684d5
Antonio Junior <jrrnet@yahoo.com.br>
2016-02-12 01:35:41 -0300

    Class UnidadeDeMedida
    Foi criado a classe UnidadeDeMedida para cada camada Modelo, DAL e BLL
    12/02//2016

---
+++ BLL/BLLUnidadeDeMedida.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using DAL;
+using Modelo;
+
+namespace BLL
+{
+    public class BLLUnidadeDeMedida
+    {
+        private DALConexao conexao;
+
+        public BLLUnidadeDeMedida(DALConexao cx)
+        {
+            this.conexao = cx;
+        }
+
+        // Incluir
+        public void Incluir(ModeloUnidadeDeMedida modelo)
+        {
+            if (modelo.UmedNome.Trim().Length == 0)
+            {
+                throw new Exception("O nome da Unidade de Medida é obrigatorio");
+            }
+
+            DALUnidadeDeMedida DALobj = new DALUnidadeDeMedida(conexao);
+            DALobj.Incluir(modelo);
+        }
+
+        // Alterar
+        public void Alterar(ModeloUnidadeDeMedida modelo)
+        {
+            if (modelo.UmedCod <= 0)
+            {
+                throw new Exception("O código da Unidade de Medida é obrigatorio");
+            }
+
+            if (modelo.UmedNome.Trim().Length == 0)
+            {
+                throw new Exception("O nome da Unidade de Medida é obrigatorio");
+            }
+
+            DALUnidadeDeMedida DALobj = new DALUnidadeDeMedida(conexao);
+            DALobj.Alterar(modelo);
+        }
+
+        // Excluir
+        public void Excluir(int codigo)
+        {
+            DALUnidadeDeMedida DALobj = new DALUnidadeDeMedida(conexao);
+            DALobj.Excluir(codigo);
+        }
+
+        // Localizar
+        public DataTable Localizar(String valor)
+        {
+            DALUnidadeDeMedida DALobj = new DALUnidadeDeMedida(conexao);
+            return DALobj.Localizar(valor);
+        }
+
+        // Carrega Modelo
+        public ModeloUnidadeDeMedida CarregaModeloUnidadeDeMedida(int codigo)
+        {
+            DALUnidadeDeMedida DALobj = new DALUnidadeDeMedida(conexao);
+            return DALobj.CarregaModeloUnidadeDeMedida(codigo);
+        }
+    }
+}
---
+++ DAL/DALUnidadeDeMedida.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using Modelo;
+
+namespace DAL
+{
+    public class DALUnidadeDeMedida
+    {
+        private DALConexao conexao;
+
+        public DALUnidadeDeMedida(DALConexao cx)
+        {
+            this.conexao = cx;
+        }
+
+        // Incluir
+        public void Incluir(ModeloUnidadeDeMedida modelo)
+        {
+            try
+            {
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = conexao.ObjetoConexao;
+                cmd.CommandText = "INSERT INTO undmedida(umed_nome) VALUES (@nome); SELECT @@IDENTITY;";
+                cmd.Parameters.AddWithValue("@nome", modelo.UmedNome);
+                conexao.Conectar();
+                modelo.UmedCod = Convert.ToInt32(cmd.ExecuteScalar());
+            }
+            catch (Exception erro)
+            {                
+                throw new Exception(erro.Message);
+            }
+            finally
+            {
+                conexao.Desconectar();
+            }
+        }
+
+        // Alterar
+        public void Alterar(ModeloUnidadeDeMedida modelo)
+        {
+            try
+            {
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = conexao.ObjetoConexao;
+                cmd.CommandText = "UPDATE undmedida SET umed_nome = @nome WHERE umed_cod = @codigo;";
+                cmd.Parameters.AddWithValue("@nome", modelo.UmedNome);
+                cmd.Parameters.AddWithValue("@codigo", modelo.UmedCod);
+
+                conexao.Conectar();
+                cmd.ExecuteNonQuery();
+            }
+            catch (Exception erro)
+            {
+                
+                throw new Exception(erro.Message);
+            }
+            finally
+            {
+                conexao.Desconectar();
+            }
+        }
+
+        // Excluir
+        public void Excluir(int codigo)
+        {
+            try
+            {
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = conexao.ObjetoConexao;
+                cmd.CommandText = "DELETE FROM undmedida WHERE umed_cod = @codigo;";
+                cmd.Parameters.AddWithValue("@codigo", codigo);
+                conexao.Conectar();
+                cmd.ExecuteNonQuery();
+            }
+            catch (Exception erro)
+            {
+                
+                throw new Exception(erro.Message);
+            }
+            finally
+            {
+                conexao.Desconectar();
+            }
+        }
+
+        // Localizar
+        public DataTable Localizar(String valor)
+        {
+            DataTable tabela = new DataTable();
+            SqlDataAdapter da = new SqlDataAdapter("SELECT * FROM undmedida WHERE umed_nome LIKE '%" + valor + "%'", conexao.StringConexao);
+            da.Fill(tabela);
+            return tabela;
+        }
+
+        public ModeloUnidadeDeMedida CarregaModeloUnidadeDeMedida(int codigo)
+        {
+            ModeloUnidadeDeMedida modelo = new ModeloUnidadeDeMedida();
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = conexao.ObjetoConexao;
+            cmd.CommandText = "SELECT * FROM undmedida WHERE umed_cod = @codigo";
+            cmd.Parameters.AddWithValue("@codigo", codigo);
+            conexao.Conectar();
+            SqlDataReader registro = cmd.ExecuteReader();
+
+            if (registro.HasRows)
+            {
+                registro.Read();
+                modelo.UmedCod = Convert.ToInt32(registro["umed_cod"]);
+                modelo.UmedNome = Convert.ToString(registro["umed_nome"]);
+            }
+            conexao.Desconectar();
+            return modelo;
+        }
+    }
+}
